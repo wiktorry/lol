ada1f7b447ef335753a8d2e1c1ea59a1df5c7049
Wiktor <wiktorszulc4@gmail.com>
2024-04-29 16:58:59 +0200

    feat: add Match and Player entity

--- src/main/java/com/example/lol/entity/Match.java
+++ src/main/java/com/example/lol/entity/Match.java
@@ -1,4 +1,24 @@
 package com.example.lol.entity;
 
-public class Game {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "matches")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Match {
+    @Id
+    @Column(name = "id")
+    private String id;
+    @ManyToMany(mappedBy = "matches")
+    List<Player> players;
+
 }
--- src/main/java/com/example/lol/entity/Player.java
+++ src/main/java/com/example/lol/entity/Player.java
@@ -1,2 +1,32 @@
-package com.example.lol.entity;public class Player {
+package com.example.lol.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "players")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Player {
+    @Id
+    @Column(name = "id")
+    private String id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "tag")
+    private String tag;
+
+    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "PlayerMatch",
+            joinColumns = {@JoinColumn(name = "playerId")},
+            inverseJoinColumns = {@JoinColumn(name = "matchId")})
+    private List<Match> matches;
+
 }
